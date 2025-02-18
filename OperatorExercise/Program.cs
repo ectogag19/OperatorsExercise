fa45869dc0d43a2436fbbb143c461f684b4e8111
George Garcia <georgegarcia05012001@gmail.com>
2025-02-18 11:11:16 -0600

    initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            
+            int sum = a + b;
+            int difference = sum - b;
+            int product = a * b;
+            
+            int quotient = a / b;
+            int remainder = a % b;
+            
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
+            
+            PrintAreaOfCircle();
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public static void PrintAreaOfCircle()
+        {
+            Console.WriteLine("What is the area of your circle?");
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {Math.Round(AreaOfCircle(radius), 3)}");
         }
     }
 }
